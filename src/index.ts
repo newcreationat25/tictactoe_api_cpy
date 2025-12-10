f8f2f0bfd73fb9e87b08e626118e0d0b8f1a05cb
newcreationat25 <newcreationat25@gmail.com>
2025-12-10 07:06:30 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", appRoute);
+app.use("/analytics", analyticsRoute);
 
 app.get("/", (req, res) => {
   res.send("TypeScript Server is Running!!");
 });
 
-app.use("/api/auth", appRoute);
-app.use("/analytics", analyticsRoute);
+
 export const handler = app;
+
 
 
 
